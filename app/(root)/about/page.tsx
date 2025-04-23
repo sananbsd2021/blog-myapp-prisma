3c09fb8d3e6929e23a4caa4534669052fe5ee02d
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2025-04-23 13:44:45 +0700

    Update page.tsx

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <div>
-      <h1 className='text-3xl font-bold bg-blue-500'>About Pahe</h1>
+      <h1 className='text-3xl font-bold bg-blue-500'>About Page</h1>
     </div>
   );
 }
